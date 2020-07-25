1bbe83646d223eaf96fe241e6aedb7296df65ae3
Taewoo Lee <lteawoo@naver.com>
2020-07-25 13:18:12 +0900

    get to post

--- src/main/java/kr/taeu/slackbot/controller/BotContorller.java
+++ src/main/java/kr/taeu/slackbot/controller/BotContorller.java
@@ -20,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 public class BotContorller {
   private final MethodsClient methodsClient;
   
-  @GetMapping("/callapi")
+  @PostMapping("/callapi")
   public String callApi() {
     
     ChatPostMessageRequest request = ChatPostMessageRequest.builder()
