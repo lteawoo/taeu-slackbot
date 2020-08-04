171092edb882f240c4971f036b04c9740e6dd6f9
이태우 <twlee@foodtechkorea.com>
2020-08-04 11:32:53 +0900

    lineBotGroup

--- src/main/java/kr/taeu/slackbot/model/LineBotGroup.java
+++ src/main/java/kr/taeu/slackbot/model/LineBotGroup.java
@@ -1,5 +1,7 @@
 package kr.taeu.slackbot.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 import lombok.Getter;
 
 @Getter
@@ -7,6 +9,7 @@ public enum LineBotGroup {
     DEV1("Cf5dc393dbfc047212f20cb10f622baee"),
     DEV2("Cf5dc393dbfc047212f20cb10f622baee");
     
+    @JsonValue  
     private String groupId;
     
     LineBotGroup(String groupId) {
