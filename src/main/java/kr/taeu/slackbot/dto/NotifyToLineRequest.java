d1c8e910552b57cec1c742b64c7a6e153577bbd0
이태우 <twlee@foodtechkorea.com>
2020-08-04 11:18:55 +0900

    formatter

--- src/main/java/kr/taeu/slackbot/dto/NotifyToLineRequest.java
+++ src/main/java/kr/taeu/slackbot/dto/NotifyToLineRequest.java
@@ -1,5 +1,6 @@
 package kr.taeu.slackbot.dto;
 
+import kr.taeu.slackbot.model.LineBotGroup;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -7,11 +8,11 @@ import lombok.ToString;
 @Getter
 @ToString
 public class NotifyToLineRequest {
-    private String lineGroupId;
+    private LineBotGroup lineGroupId;
     private String message;
     
     @Builder
-    public NotifyToLineRequest(String lineGroupId, String message) {
+    public NotifyToLineRequest(LineBotGroup lineGroupId, String message) {
         this.lineGroupId = lineGroupId;
         this.message = message;
     }
